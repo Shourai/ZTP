1d94aaddb7839aa594fd3f9dd54de57e28bd6a69
Shourai <10200748+Shourai@users.noreply.github.com>
2020-09-27 21:41:16 +0200

    Initial commit

---
+++ designer.py
@@ -0,0 +1,14 @@
+import csv
+from jinja2 import Template, StrictUndefined
+
+with open('./kea-dhcp4.conf') as g:
+    template = Template(g.read(), undefined=StrictUndefined)
+
+with open('info.csv') as f:
+    data = csv.DictReader(f, delimiter=' ', skipinitialspace=True)
+    data = list(data)
+
+with open('output.txt', 'w') as h:
+    h.write(template.render(hosts=data))
+    
+print(template.render(hosts=data))
